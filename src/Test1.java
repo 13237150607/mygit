baaabc392c5c0261fc2e8cab6e2f4d093730ce69
xyh <m13237150607@163.com>
2020-03-16 15:05:10 +0800

    张三

---
+++ src/Test1.java
@@ -0,0 +1,9 @@
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("111111111111111");
+    }
+
+    public void method1(){
+        System.out.println(123);
+    }
+}
